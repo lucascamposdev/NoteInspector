3d96f656c9770d0dac1d0812197064e99223b9da
Lucas <prestoinker@gmail.com>
2024-09-27 22:54:29 -0300

    feat: Table View Screen

---
+++ TableViewScreen.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace NoteInspector
+{
+    partial class TableViewScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TableViewScreen));
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lbl_tableName = new System.Windows.Forms.Label();
+            this.tableView = new System.Windows.Forms.DataGridView();
+            this.input_buscar = new System.Windows.Forms.TextBox();
+            this.btn_Filtrar = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tableView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Controls.Add(this.btn_Filtrar);
+            this.panel1.Controls.Add(this.input_buscar);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.lbl_tableName);
+            this.panel1.Location = new System.Drawing.Point(1, -1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1537, 63);
+            this.panel1.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(20, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(20, 20);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lbl_tableName
+            // 
+            this.lbl_tableName.AutoSize = true;
+            this.lbl_tableName.Font = new System.Drawing.Font("Verdana", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_tableName.ForeColor = System.Drawing.Color.White;
+            this.lbl_tableName.Location = new System.Drawing.Point(46, 19);
+            this.lbl_tableName.Name = "lbl_tableName";
+            this.lbl_tableName.Size = new System.Drawing.Size(90, 28);
+            this.lbl_tableName.TabIndex = 0;
+            this.lbl_tableName.Text = "Coelba";
+            // 
+            // tableView
+            // 
+            this.tableView.AllowUserToOrderColumns = true;
+            this.tableView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.DisplayedCells;
+            this.tableView.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.tableView.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.DarkSlateGray;
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.tableView.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.tableView.ColumnHeadersHeight = 25;
+            this.tableView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.tableView.DefaultCellStyle = dataGridViewCellStyle2;
+            this.tableView.EnableHeadersVisualStyles = false;
+            this.tableView.GridColor = System.Drawing.SystemColors.ButtonFace;
+            this.tableView.Location = new System.Drawing.Point(1, 62);
+            this.tableView.Name = "tableView";
+            this.tableView.RowHeadersVisible = false;
+            this.tableView.RowHeadersWidth = 51;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tableView.RowsDefaultCellStyle = dataGridViewCellStyle3;
+            this.tableView.RowTemplate.Height = 24;
+            this.tableView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.tableView.Size = new System.Drawing.Size(1537, 581);
+            this.tableView.TabIndex = 22;
+            // 
+            // input_buscar
+            // 
+            this.input_buscar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.input_buscar.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.input_buscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.input_buscar.Location = new System.Drawing.Point(1187, 20);
+            this.input_buscar.Name = "input_buscar";
+            this.input_buscar.Size = new System.Drawing.Size(213, 27);
+            this.input_buscar.TabIndex = 23;
+            this.input_buscar.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.input_buscar_KeyPress);
+            // 
+            // btn_Filtrar
+            // 
+            this.btn_Filtrar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.btn_Filtrar.BackColor = System.Drawing.Color.Black;
+            this.btn_Filtrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Filtrar.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Filtrar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn_Filtrar.Location = new System.Drawing.Point(1412, 18);
+            this.btn_Filtrar.Name = "btn_Filtrar";
+            this.btn_Filtrar.Size = new System.Drawing.Size(103, 29);
+            this.btn_Filtrar.TabIndex = 24;
+            this.btn_Filtrar.Text = "Filtrar";
+            this.btn_Filtrar.UseVisualStyleBackColor = false;
+            this.btn_Filtrar.Click += new System.EventHandler(this.btn_Filtrar_Click);
+            // 
+            // TableViewScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1537, 641);
+            this.Controls.Add(this.tableView);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "TableViewScreen";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Table View";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tableView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lbl_tableName;
+        private System.Windows.Forms.DataGridView tableView;
+        private System.Windows.Forms.TextBox input_buscar;
+        private System.Windows.Forms.Button btn_Filtrar;
+    }
+}
---
+++ TableViewScreen.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NoteInspector
+{
+    public partial class TableViewScreen : Form
+    {
+        private DataTable _data;
+        public TableViewScreen(string tableName, DataTable data)
+        {
+            InitializeComponent();
+            _data = data;
+            tableView.DataSource = data;
+            lbl_tableName.Text = tableName;
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (keyData == Keys.Escape)
+            {
+                this.Close(); 
+                return true; 
+            }
+
+            return base.ProcessCmdKey(ref msg, keyData); 
+        }
+
+        private void btn_Filtrar_Click(object sender, EventArgs e)
+        {
+            string searchValue = input_buscar.Text;
+
+            if (string.IsNullOrEmpty(searchValue))
+            {
+                tableView.DataSource = _data; 
+                return;
+            }
+
+            string filterExpression = "";
+            foreach (DataColumn column in _data.Columns)
+            {
+                if (column.DataType == typeof(string) || column.DataType == typeof(int) || column.DataType == typeof(double) || column.DataType == typeof(decimal))
+                {
+                    if (!string.IsNullOrEmpty(filterExpression))
+                        filterExpression += " OR ";
+                    filterExpression += $"CONVERT([{column.ColumnName}], 'System.String') LIKE '%{searchValue}%'";
+                }
+            }
+
+            DataView dv = new DataView(_data);
+            dv.RowFilter = filterExpression;
+
+            tableView.DataSource = dv;
+
+            if (dv.Count == 0)
+            {
+                MessageBox.Show("Nenhum valor encontrado.");
+            }
+        }
+
+        private void input_buscar_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                btn_Filtrar_Click(sender, EventArgs.Empty);
+
+                e.Handled = true; 
+            }
+        }
+    }
+}
